17e938448e71e5a2799e3ef3bef13117629d5d64
Alan Cruz <cruz.alan@gmail.com>
2021-06-05 21:08:21 -0400

    package restructure

--- src/converter/server.js
+++ src/converter/server.js
@@ -10,16 +10,16 @@ app.get("/rgbToHex", function(req, res) {
   var green = parseInt(req.query.green, 10);
   var blue  = parseInt(req.query.blue, 10);
   var hex = converter.rgbToHex(red, green, blue);
+  
   res.send(hex);
-
 });
 
 app.get("/hexToRgb", function(req, res) {
 
   var hex = req.query.hex;
   var rgb = converter.hexToRgb(hex);
+  
   res.send(JSON.stringify(rgb));
-
 });
 
 app.listen(3000);
